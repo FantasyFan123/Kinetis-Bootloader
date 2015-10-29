7190c57e0358ac319a17d2104f8eb6df1dc6c8ca
FantasyFan123 <b55335@freescale.com>
2015-10-29 10:26:51 +0800

    Revert "Edited by Fan Xi"
    This reverts commit 9b947c064dcc41122e0ab080352d08219d921ade.

--- Kinetis Bootloader SW/Bootloader and Application for KL26/User Application Source Code/user_app_red_blink_kl26_cw/FLASH/Project_Settings/Startup_Code/kinetis_sysinit.d
+++ Kinetis Bootloader SW/Bootloader and Application for KL26/User Application Source Code/user_app_red_blink_kl26_cw/FLASH/Project_Settings/Startup_Code/kinetis_sysinit.d
@@ -4,8 +4,8 @@ Project_Settings/Startup_Code/kinetis_sysinit.o: \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
  ../Project_Settings/Startup_Code/kinetis_sysinit.h \
- C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/derivative.h \
- C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/MKL25Z4.h \
+ C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/derivative.h \
+ C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/MKL25Z4.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
@@ -23,9 +23,9 @@ C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_versio
 
 ../Project_Settings/Startup_Code/kinetis_sysinit.h:
 
-C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/derivative.h:
+C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/derivative.h:
 
-C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/MKL25Z4.h:
+C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/MKL25Z4.h:
 
 C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
 
