a8aef8082ded0c0c690ae21d84b53d1999b2dfb0
“FantasyFan123” <b55335@freescale.com>
2015-10-29 15:54:43 +0800

    123
    123

--- Kinetis Bootloader SW/Bootloader and Application for KL26/User Application Source Code/user_app_red_blink_kl26_cw/FLASH/Sources/main.d
+++ Kinetis Bootloader SW/Bootloader and Application for KL26/User Application Source Code/user_app_red_blink_kl26_cw/FLASH/Sources/main.d
@@ -2,8 +2,8 @@ Sources/main.o: ../Sources/main.c \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
- C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/derivative.h \
- C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/MKL25Z4.h \
+ C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/derivative.h \
+ C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/MKL25Z4.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
  C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
@@ -20,9 +20,9 @@ C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefi
 
 C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
 
-C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/derivative.h:
+C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/derivative.h:
 
-C:/Users/b08277/Desktop/Bootloader20150907/user_app_red_blink/Project_Headers/MKL25Z4.h:
+C:/Users/B55335/Desktop/Kinetis-Bootloader-master/Kinetis\ Bootloader\ SW/Bootloader\ and\ Application\ for\ KL26/User\ Application\ Source\ Code/user_app_red_blink_kl26_cw/Project_Headers/MKL25Z4.h:
 
 C:/Freescale/CW\ MCU\ v10.6.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
 
